d9c0c646834fd5b37c5db1d5f3b45eacdb021237
jasingizwe <c.asingizwe@alustudent.com>
2024-06-16 06:35:51 -0700

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application,
+Web application is listening on 0.0.0.0, port 5000 """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """ Displaying Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Display HBNB! """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def text_c(text):
+    """ Display C """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def text_py(text="is cool"):
+    """ Display Python """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def num_n(n):
+    """Displaying the number n"""
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.url_map.strict_slashes = False
